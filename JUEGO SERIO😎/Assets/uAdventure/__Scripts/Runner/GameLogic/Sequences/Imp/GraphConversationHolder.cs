a054882d106d2d7c4d7ee71f3a56589f064e5a20
mmur07 <mmur@ucm.es>
2020-11-12 17:33:14 +0100

    👌👌👌👌👌good shit💯💯💯💯

---
+++ JUEGO SERIO😎/Assets/uAdventure/__Scripts/Runner/GameLogic/Sequences/Imp/GraphConversationHolder.cs
@@ -0,0 +1,212 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using uAdventure.Core;
+using AssetPackage;
+
+namespace uAdventure.Runner
+{
+    public class ConversationNodeHolder
+    {
+        private ConversationNode node;
+        public int child = -2;
+        private EffectHolder additional_effects;
+        private EffectHolder end_conversation_effects;
+        private int current_line = 0;
+        private bool showOption = false;
+        private bool isTracePending = false;
+        private string xAPIQuestion;
+        private string xAPIResponse;
+        private bool xAPISuccess;
+
+        public bool TracePending { get { return isTracePending; } }
+
+
+        public ConversationNodeHolder(ConversationNode node)
+        {
+            this.node = node;
+
+            if (node != null)
+            {
+                if(node is DialogueConversationNode)
+                {
+                    var dialog = node as DialogueConversationNode;
+                    this.additional_effects = new EffectHolder(dialog.getEffects());
+                    this.child = 0;
+                }
+                else if (node is OptionConversationNode)
+                {
+                    var option = node as OptionConversationNode;
+                    this.additional_effects = new EffectHolder(option.getEffects());
+                    this.child = -2;
+                    this.showOption = option.isShowUserOption();
+                }
+            }
+        }
+
+        public bool execute()
+        {
+            bool forcewait = false;
+            ConversationLine l;
+
+            if (node != null)
+            {
+
+                if (node is DialogueConversationNode)
+                {
+                    while (current_line < node.getLineCount() && !forcewait)
+                    {
+                        l = node.getLine(current_line);
+                        if (ConditionChecker.check(l.getConditions()))
+                        {
+                            forcewait = true;
+                            Game.Instance.Talk(l, l.getName());
+                        }
+                        current_line++;
+                    }
+
+                    if (!forcewait && additional_effects != null && additional_effects.effects.Count > 0)
+                    {
+                        forcewait = additional_effects.execute();
+                    }
+                }
+                else if (node is OptionConversationNode)
+                {
+                    if (this.child == -2)
+                    {
+                        if (isTracePending)
+                        {
+                            EndTrace();
+                        }
+
+                        Game.Instance.showOptions(this);
+                        forcewait = true;
+                    }
+                    else if (showOption)
+                    {
+                        l = node.getLine(child);
+                        Game.Instance.Talk(l, l.getName());
+                        forcewait = true;
+                        showOption = false;
+                    }
+                    else
+                    {
+                        if (additional_effects != null)
+                            forcewait = additional_effects.execute();
+                        else
+                            forcewait = false;
+                    }
+                }
+            }
+
+            return forcewait;
+        }
+
+        public void clicked(int option)
+        {
+            this.child = option;
+            OptionConversationNode onode = ((OptionConversationNode)node);
+            if(option == onode.getChildCount()) // Timeout
+            {
+                // TODO Analytics for timeout
+                return;
+            }
+
+            if (TrackerAsset.Instance.Started && !string.IsNullOrEmpty(onode.getXApiQuestion()))
+            {
+                isTracePending = true;
+                Game.Instance.GameState.BeginChangeAmbit();
+                xAPISuccess = onode.getLine(option).getXApiCorrect();
+                xAPIQuestion = onode.getXApiQuestion();
+                xAPIResponse = onode.getLine(option).getText().Replace(",", " ");
+            }
+        }
+
+        public void EndTrace()
+        {
+            if (!TrackerAsset.Instance.Started)
+            {
+                return;
+            }
+
+            Game.Instance.GameState.EndChangeAmbitAsExtensions();
+            TrackerAsset.Instance.setSuccess(xAPISuccess);
+            TrackerAsset.Instance.Alternative.Selected(xAPIQuestion, xAPIResponse, AlternativeTracker.Alternative.Question);
+            TrackerAsset.Instance.Flush();
+        }
+
+        public ConversationNodeHolder getChild()
+        {
+            return (node != null) ? new ConversationNodeHolder(node.getChild(this.child)) : null;
+        }
+
+        public ConversationNode getNode()
+        {
+            return node;
+        }
+    }
+
+    public class GraphConversationHolder : Secuence
+    {
+        private List<ConversationNodeHolder> nodes;
+
+        public GraphConversationHolder(Conversation conversation)
+        {
+            this.nodes = new List<ConversationNodeHolder>();
+
+            foreach (ConversationNode node in conversation.getAllNodes())
+            {
+                nodes.Add(new ConversationNodeHolder(node));
+            }
+        }
+
+        private ConversationNodeHolder current;
+        private ConversationNodeHolder tracePendingNode;
+        public bool execute()
+        {
+            bool forcewait = false;
+
+            if (current == null)
+                current = nodes[0];
+
+            while (!forcewait)
+            {
+                if (current.execute())
+                {
+                    forcewait = true;
+                    break;
+                }
+                else
+                {
+                    ConversationNodeHolder child = current.getChild();
+                    if (current.TracePending)
+                    {
+                        // If the current node has a pending trace
+                        // We end the previous trace pending
+                        if (tracePendingNode != null)
+                            tracePendingNode.EndTrace();
+
+                        // And we put this into pending
+                        tracePendingNode = current;
+                    }
+
+                    current = child;
+                    if (current == null)
+                    {
+                        // When the conversation is over if there's a
+                        // pending node, we end it
+                        if(tracePendingNode != null)
+                            tracePendingNode.EndTrace();
+                        break;
+                    }
+                }
+            }
+
+            if (!forcewait)
+                this.current = null;
+
+            return forcewait;
+        }
+    }
+}
---
+++ JUEGO SERIO😎/Assets/uAdventureAnalytics/Plugins/Simva/SimvaConf.cs
@@ -0,0 +1,146 @@
+﻿using SimpleJSON;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Simva
+{
+    public class SimvaConf
+    {
+        private static SimvaConf local;
+
+        public static SimvaConf Local
+        {
+            get
+            {
+                return local;
+            }
+            set
+            {
+                local = value;
+            }
+        }
+
+        public string Host { get; set; }
+
+        public string Port { get; set; }
+
+        public string Protocol { get; set; }
+
+        public string SSO { get; private set; }
+
+        public string ClientId { get; set; }
+
+        public string Study { get; set; }
+
+        public string URL
+        {
+            get
+            {
+                return Protocol + "://" + Host + ":" + Port;
+            }
+        }
+
+        public bool TraceStorage { get; set; }
+        public bool Backup { get; set; }
+        public bool Realtime { get; set; }
+
+        public SimvaConf()
+        {
+            Debug.Log("[SIMVA CONF] Loading...");
+            Host = "localhost";
+            Port = "443";
+            Protocol = "https";
+        }
+
+        public IEnumerator LoadAsync()
+        {
+            string contents = "";
+
+            // WebGL and Android have to use WWW to load from streaming assets
+#if UNITY_WEBPLAYER || UNITY_WEBGL || UNITY_ANDROID 
+            Debug.Log("[SIMVA CONF] Doing WebGL / Android read...");
+            UnityWebRequest reader = GetReader();
+            yield return reader.SendWebRequest();
+            if (string.IsNullOrEmpty(reader.error))
+            {
+                Debug.Log("[SIMVA CONF] Request failed: (" + reader.responseCode + " ) " + reader.error + " - " + reader.downloadHandler.data);
+                contents = System.Text.Encoding.UTF8.GetString(reader.downloadHandler.data);
+            }
+#else       // The others can read from System.IO       
+            string filePath = GetFilePath();     
+            if (System.IO.File.Exists(filePath))
+            {
+                contents = System.IO.File.ReadAllText(filePath);
+            }
+            yield return null;
+#endif
+            ParseContents(contents);
+        }
+
+        private void ParseContents(string contents)
+        {
+            if (!string.IsNullOrEmpty(contents))
+            {
+                Debug.Log("[SIMVA CONF] Simva.conf content: " + contents);
+                var simvaconf = SimpleJSON.JSON.Parse(contents);
+                Study = simvaconf["study"];
+                Host = simvaconf["host"];
+                Protocol = simvaconf["protocol"];
+                Port = simvaconf["port"];
+                SSO = simvaconf["sso"];
+                ClientId = simvaconf["client_id"];
+                TraceStorage = simvaconf["trace_storage"].AsBool;
+                Backup = simvaconf["backup"].AsBool;
+                Realtime = simvaconf["realtime"].AsBool;
+            }
+        }
+
+        private static UnityWebRequest GetReader()
+        {
+            var FileName = "simva.conf";
+            UnityWebRequest reader = null;
+            // Platform dependent StreamingAssets Load https://docs.unity3d.com/Manual/StreamingAssets.html
+#if UNITY_WEBPLAYER || UNITY_WEBGL
+            reader = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + FileName);
+#elif UNITY_ANDROID
+            reader = new UnityWebRequest.Get("jar: file://" + Application.dataPath + "!/assets/" + FileName);
+#endif
+            Debug.Log("[SIMVA CONF] Requesting simva.conf from: " + reader.uri);
+            return reader;
+        }
+
+        private static string GetFilePath()
+        {
+            var FileName = "simva.conf";
+
+            // Platform dependent StreamingAssets Load https://docs.unity3d.com/Manual/StreamingAssets.html
+#if UNITY_IOS
+            var filePath = Path.Combine(Application.dataPath + "/Raw", FileName);
+#else
+            var filePath = System.IO.Path.Combine(Application.streamingAssetsPath + "/", FileName);
+#endif
+            return filePath;
+        }
+
+        public void Save()
+        {
+            var path = GetFilePath();
+            if (Application.isEditor && !Application.isPlaying)
+            {
+                var simvaconf = new SimpleJSON.JSONClass();
+                simvaconf["study"] = Study;
+                simvaconf["host"] = Host;
+                simvaconf["protocol"] = Protocol;
+                simvaconf["port"] = Port;
+                simvaconf["sso"] = SSO;
+                simvaconf["client_id"] = ClientId;
+                simvaconf["url"] = URL;
+                simvaconf["trace_storage"] = TraceStorage.ToString();
+                simvaconf["backup"] = Backup.ToString();
+                simvaconf["realtime"] = Realtime.ToString();
+                System.IO.File.WriteAllText(path, simvaconf.ToJSON(4));
+            }
+        }
+    }
+}
